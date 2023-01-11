d9325372a9eca535c075d2a55fe301867b047ff2
SamayaA <asadullaevasamaya@gmail.com>
2023-01-11 23:09:17 +0300

    add onject GameSaving

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,22 +1,33 @@
 import read from './parser';
 import json from './reader';
 
+class GameSaving {
+  constructor(data) {
+    this.id = data.id;
+    this.created = data.created;
+    this.userInfo = {
+      id: data.userInfo.id,
+      name: data.userInfo.name,
+      level: data.userInfo.level,
+      points: data.userInfo.points,
+    };
+  }
+}
+
 export default class GameSavingLoader {
   static load() {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        read()
-          .then((buffer) => {
-            json(buffer)
-              .then((data) => {
-                resolve(data);
-              });
-          })
-          .catch((error) => {
-            reject(error);
-          });
+    return read()
+      .then((response) => (json(response)))
+      .then((response) => {
+        const buffer = new Uint16Array(response);
+        let result = '';
+        let i = 0;
+        while (buffer[i]) {
+          result += String.fromCharCode(buffer[i]);
+          i++;
+        }
+        // console.log('GameSaving object', JSON.parse(result));
+        return new GameSaving(JSON.parse(result));
       });
-    }, 0);
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,3 @@
 import GameSavingLoader from './GameSavingLoader';
 
-GameSavingLoader.load().then((data) => {
-  const buffer = new Uint16Array(data);
-  let result = '';
-  let i = 0;
-  while (buffer[i]) {
-    result += String.fromCharCode(buffer[i]);
-    i++;
-  }
-  console.log(1, result);
-}, (error) => {
-  console.log(error);
-});
+GameSavingLoader.load().then((response) => console.log(response), (error) => console.log(error));
